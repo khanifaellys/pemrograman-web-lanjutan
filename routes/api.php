a7ca6557cbe312cba79ba1d126d045e678938834
khanifaellys <khanifaellys@gmail.com>
2020-02-25 03:38:38 -0800

    autentikasi API JWT

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,26 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::apiResource('artikel','ArtikelAPIController');
+Route::group([
 
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthApiController@login');
+    Route::post('logout', 'AuthApiController@logout');
+    Route::post('refresh', 'AuthApiController@refresh');
+    Route::post('me', 'AuthApiController@me');
+
+});
+
+
+
+
+
+Route::apiResource('artikel','ArtikelAPIController');
+Route::apiResource('kategoriArtikel','KategoriArtikelAPIController');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
